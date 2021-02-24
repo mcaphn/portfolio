540148f790ced99ad69bdefcf900ea47591154e8
mcaphn <mica.phan@gmail.com>
2021-02-24 10:23:51 -0500

    fix sidebar nav
    created away() to remove sidebar when window display is 750px min

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const toggleBtn = document.querySelector(".toggle");
 const sidebar = document.querySelector(".sidebar");
+const windowWidth = window.matchMedia("(min-width: 750px)");
 
 toggleBtn.addEventListener('click', function() {
   sidebar.classList.toggle("show-sidebar");
   toggleBtn.classList.toggle("toggle-spin");
 });
+
+function away(windowWidth) {
+  if (windowWidth.matches && sidebar.classList.contains("show-sidebar")) {
+      sidebar.classList.toggle("show-sidebar");
+      toggleBtn.classList.toggle("toggle-spin");
+  }
+}
+
+
+away(windowWidth);
+windowWidth.addListener(away);
